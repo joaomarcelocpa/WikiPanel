5be93493ab091bf88306f26ad1a83614c40f7b4e
João Marcelo <joaomarcelocpa0303@gmail.com>
2025-10-16 09:21:50 -0300

    adjusts

--- src/shared/services/information.service.ts
+++ src/shared/services/information.service.ts
@@ -4,7 +4,7 @@ import type {
     SubCategoryResponse,
 } from '../interfaces/information.interface';
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
 
 function getPublicHeaders(): HeadersInit {
     return {
